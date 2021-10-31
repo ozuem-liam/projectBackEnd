6a8b625a10727997e798908ece542834cc230b3c
Williams Ozuem <ozuemdw@gmail.com>
2021-10-31 21:53:29 +0100

    Update EmailClient.ts

--- src/utils/EmailClient.ts
+++ src/utils/EmailClient.ts
@@ -1,7 +1,6 @@
 import mailgun from "mailgun-js";
-const API_KEY = 'ec97422f9a6aab7ac15ad474ec3ba542-90346a2d-032b6265';
+const API_KEY = 
 const DOMAIN =
-  'sandbox455465d101ea46bab58a06820fda4409.mailgun.org';
 const mg = mailgun({
   apiKey: API_KEY,
   domain: DOMAIN,
